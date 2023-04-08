295f74636fe6f4a181fbf9289ff8591512879622
Quentin Guidée <quentin.guidee@gmail.com>
2023-04-08 14:28:52 -0400

    Cleanup Doxygen output to keep only .dot files
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- doxygen/doxygen.go
+++ doxygen/doxygen.go
@@ -1,8 +1,11 @@
 package doxygen
 
 import (
+	"log"
 	"os"
 	"os/exec"
+	"path"
+	"path/filepath"
 )
 
 func Run() error {
@@ -12,5 +15,37 @@ func Run() error {
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
 
-	return cmd.Run()
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+
+	err = cleanup()
+	return err
+}
+
+// cleanup deletes all useless files
+func cleanup() error {
+	entries, err := os.ReadDir("linter-dot")
+	if err != nil {
+		return err
+	}
+
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
+
+		ext := filepath.Ext(entry.Name())
+
+		if ext != ".dot" {
+			err := os.Remove(path.Join("linter-dot", entry.Name()))
+			if err != nil {
+				log.Fatalf("%v", err)
+				return err
+			}
+		}
+	}
+
+	return nil
 }
